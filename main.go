808fbeda696f5c58bb0da5d509be5bd9e84d43c9
Sunil Kayiti <skayiti@skayiti.lan>
2019-07-20 13:57:06 -0700

    created view templates using go template library

--- main.go
+++ main.go
@@ -1,24 +1,45 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"html/template"
 
 	"github.com/gorilla/mux"
 )
 
+var(
+	homeTemplate *template.Template
+	contactTemplate *template.Template
+) 
+
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Yo! Welcome to Go World</h1>")
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, `To get in touch please send an email to 
-	<a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>`)
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
+
 }
 
 func main() {
+	var err error
+	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
