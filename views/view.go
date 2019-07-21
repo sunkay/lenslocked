e5decd4130dc751c47573ed27440e1eca20a6ca6
sunkay <sunkay11@gmail.com>
2019-07-21 09:41:25 -0700

    refactor to use templates for views

--- views/view.go
+++ views/view.go
@@ -2,8 +2,10 @@ package views
 
 import "html/template"
 
-func NewView(files ...string) *View {
-	files = append(files, "views/layouts/footer.gohtml")
+func NewView(layout string, files ...string) *View {
+	files = append(files,
+		"views/layouts/footer.gohtml",
+		"views/layouts/bootstrap.gohtml")
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
@@ -11,9 +13,12 @@ func NewView(files ...string) *View {
 
 	return &View{
 		Template: t,
+		Layout:   layout,
 	}
 }
 
 type View struct {
 	Template *template.Template
+	Layout   string
 }
+
