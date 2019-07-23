a25ba4deb5a7bac2877a15a16ff8ba55bccea8a5
sunkay <sunkay11@gmail.com>
2019-07-23 15:22:01 -0700

    refactor views to make it cleaner and simpler

--- controllers/static.go
+++ controllers/static.go
@@ -4,8 +4,8 @@ import "lenslocked.com/views"
 
 func NewStatic() *Static {
 	return &Static{
-		Home:    views.NewView("bootstrap", "views/static/home.gohtml"),
-		Contact: views.NewView("bootstrap", "views/static/contact.gohtml"),
+		Home:    views.NewView("bootstrap", "static/home"),
+		Contact: views.NewView("bootstrap", "static/contact"),
 	}
 }
 
