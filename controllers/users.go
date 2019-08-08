b83915d2174798776fa57ea4b3711af5656c6556
sunkay <sunkay11@gmail.com>
2019-08-08 07:38:19 -0400

    add login form

--- controllers/users.go
+++ controllers/users.go
@@ -23,6 +23,11 @@ type Users struct {
 	us        *models.UserService
 }
 
+// New is used to render the form where a user can
+// create a new user account.
+//
+// GET /signup
+
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	if err := u.NewView.Render(w, nil); err != nil {
 		panic(err)
@@ -34,6 +39,11 @@ type SignupForm struct {
 	Email    string `schema:"email"`
 	Password string `schema:"password"`
 }
+
+// Create is used to process the signup form when a user
+// tries to create a new user account.
+//
+// POST /signup
 
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	var form SignupForm
